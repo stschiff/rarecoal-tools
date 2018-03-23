643ef19c2ec015616cad2ebb3b9f146fa861ecb2
Stephan Schiffels <schiffels_schiffels@mac.com>
2018-03-23 12:40:48 +0100

    filterFreqSum doesn't work still, will have to go back to previous mode of cycling through freqSum and bedFile simultaneously

--- src-filterFreqSum/filterFreqSum.hs
+++ src-filterFreqSum/filterFreqSum.hs
@@ -5,12 +5,14 @@ import SequenceFormats.FreqSum (FreqSumEntry(..), readFreqSumStdIn, printFreqSum
 import SequenceFormats.Utils (liftParsingErrors)
 import Control.Applicative (optional)
 import Control.Monad.Trans.Class (lift)
+import Control.Monad (void)
 import Data.Char (isSpace)
 import Data.Maybe (catMaybes)
 import Data.Text (unpack, Text)
 import qualified Data.Attoparsec.Text as A
+import Debug.Trace (trace)
 import Pipes (Producer, runEffect, yield, (>->), next, Pipe, cat, for)
-import Pipes.Attoparsec (parsed)
+import Pipes.Attoparsec (parsed, ParsingError)
 import qualified Pipes.Prelude as P
 import Pipes.Safe (runSafeT)
 import qualified Pipes.Text.IO as PT
@@ -28,11 +30,15 @@ argParser = (,) <$>
 main :: IO ()
 main = do
     (maybeBedFile, missingness) <- options "script to filter a freqSum file" argParser
+    -- let (Just bedFile) = maybeBedFile
+    --     textProd = PT.readFile . unpack . format fp $ bedFile
+    --     bedProd = parsed bedFileParser textProd >>= liftParsingErrors
+
     let bedFilter = case maybeBedFile of
             Nothing -> cat
             Just bedFile ->
                 let textProd = PT.readFile . unpack . format fp $ bedFile
-                    bedProd = parsed bedFileParser textProd >>= liftParsingErrors
+                    bedProd = parsed bedFileParser textProd
                 in  filterThroughBed bedProd
     runSafeT $ do
         (fsHeader, fsBody) <- readFreqSumStdIn
@@ -40,12 +46,18 @@ main = do
             P.filter (missingnessFilter missingness (fshCounts fsHeader)) >->
             printFreqSumStdOut fsHeader
 
-filterThroughBed :: (Monad m) => Producer BedEntry m () -> Pipe FreqSumEntry FreqSumEntry m ()
+filterThroughBed :: (MonadIO m) =>
+    Producer BedEntry m (Either (ParsingError, Producer Text m ()) ()) ->
+    Pipe FreqSumEntry FreqSumEntry m ()
 filterThroughBed bedProd = do
     b <- lift $ next bedProd
-    let (bedCurrent, bedRest) = case b of
-            Left _ -> error "Bed file empty or not readable"
-            Right r -> r
+    (bedCurrent, bedRest) <- case b of
+        Left (Left (pErr, restProd)) -> do
+            Right (chunk, _) <- lift $ next restProd
+            error $ "1Parsing Error in bed file: " ++ show pErr ++
+                    ", which occurred while trying to parse this bit: " ++ show chunk
+        Left (Right ()) -> error "Error: Empty Bed File"
+        Right r -> return r
     for cat (go bedCurrent bedRest)
   where
     go bedCurrent bedRest fsCurrent = do
@@ -53,11 +65,14 @@ filterThroughBed bedProd = do
             BedBehind -> do
                 b <- lift $ next bedRest
                 case b of
-                    Left () -> return ()
+                    Left (Left (pErr, restProd)) -> do
+                        Right (chunk, _) <- lift $ next restProd
+                        error $ "2Parsing Error in bed file: " ++ show pErr ++
+                                ", which occurred while trying to parse this bit: " ++ show chunk
+                    Left (Right ()) -> return ()
                     Right (nextBed, bedRest') -> go nextBed bedRest' fsCurrent
             BedAhead -> return ()
-            FSwithin -> do
-                yield fsCurrent                
+            FSwithin -> yield fsCurrent   
 
 missingnessFilter :: Double -> [Int] -> FreqSumEntry -> Bool
 missingnessFilter m hapNums fs =
